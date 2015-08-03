e0caba4c45d3adab3a0854031d51040013d0697f
luvwahraan <Luvwahraan+github@pikaed.org>
2015-08-03 14:36:49 +0200

    Premier ajout.

---
+++ LKSync.pl
@@ -0,0 +1,14 @@
+#!/usr/env perl
+
+use warning;
+use strict;
+
+my $login = 'Oslarth';
+my $password = 'UnBocageTueDesMouches';
+
+my $API = 'http://leekwars.com/api';
+my %api = (
+    login => '/farmer/login',
+  );
+
+
